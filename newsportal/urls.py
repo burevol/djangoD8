abe7b083585d0cec7d585137ffa3638cfcf5d588
alexum <alexum2013@yandex.ru>
2021-12-09 20:10:47 +0300

    Добавлено кэширование главной страницы и страницы категорий

--- newsportal/urls.py
+++ newsportal/urls.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from .views import PostList, PostDetail, PostCreateView, PostUpdateView, PostDeleteView, PostCategoryView, upgrade_me, \
     subscribe
 
 urlpatterns = [
-    path('', PostList.as_view()),
+    path('', cache_page(60)(PostList.as_view())),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('add/', PostCreateView.as_view(), name='post_create'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
     path('upgrade/', upgrade_me, name='upgrade'),
-    path('category/<int:pk>', PostCategoryView.as_view(), name='post_category'),
+    path('category/<int:pk>', cache_page(60*5)(PostCategoryView.as_view()), name='post_category'),
     path('category/<int:pk>/subscribe', subscribe, name='subscribe'),
 ]

ff5206cc08bb228c33af8c2fa2a90276e70c6042
burevol <alexum2013@yandex.ru>
2022-02-01 19:21:36 +0300

    Upgrade admin panel

--- newsportal/admin.py
+++ newsportal/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Category, PostCategory, Author, Comment, Post
+
+from .models import Author, Category, Comment, Post
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'rating')
+    list_filter = ('user',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('header', 'rating', 'type', 'date', 'author')
+    list_filter = ('type', 'date', 'author')
+
 
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Category)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Comment)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
